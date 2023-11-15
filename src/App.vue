9ca8288fdb14c645b7675f40b07418d507430123
ShadowWolf5953 <41658832+ShadowWolf5953@users.noreply.github.com>
2023-11-14 20:27:19 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -96,7 +96,7 @@ export default {
         {
           "slug": "001",
           "name": "The-Drop",
-          "status": "Start"
+          "status": "start"
         },
       ],
       "pilots": [
