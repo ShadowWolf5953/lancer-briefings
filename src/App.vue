d838b02b2718e575bf7c7a8ad2facfbd27b0cf0d
ShadowWolf5953 <41658832+ShadowWolf5953@users.noreply.github.com>
2023-11-17 19:31:00 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -101,12 +101,12 @@ export default {
       ],
       "pilots": [
         {
-          "callsign": "test1",
-          "alias": "Shayan Arsenal",
+          "callsign": "HERMES",
+          "alias": "Zelda Bombich",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "mech": "Inauthentic Behavior"
         },
         {
           "callsign": "test2",
